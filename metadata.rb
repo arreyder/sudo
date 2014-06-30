088bf27ffb64ef7da31ca2a200c82bfb1a5c0396
Chris Rhodes <crrhodes@godaddy.com>
2014-06-30 10:12:21 -0700

    cut and paste fail

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs sudo and configures /etc/sudoers"
-+name             "sudo"
+name              "sudo"
 version           "1.0.2"
 
 recipe "sudo", "Installs sudo and configures /etc/sudoers"
