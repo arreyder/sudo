6e6350f57914b0b88bc9a1eb151a83c3dd581b83
Chris Rhodes <crrhodes@godaddy.com>
2014-06-30 10:11:08 -0700

    add name so berk stuff is happy

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs sudo and configures /etc/sudoers"
++name             "sudo"
 version           "1.0.2"
 
 recipe "sudo", "Installs sudo and configures /etc/sudoers"
